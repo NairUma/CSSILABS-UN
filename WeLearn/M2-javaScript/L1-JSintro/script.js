e6669f077321726b38f3121711dc7ad0af27aa10
Uma Nair <nair003@csusm.edu>
2019-07-24 11:59:18 -0700

    L2 - JSFunction

---
+++ WeLearn/M2-javaScript/L1-JSintro/script.js
@@ -0,0 +1,35 @@
+console.log("script running!");
+
+let name = "Charlie";
+console.log("Hello " + name +"!");
+
+let age = 16;
+console.log("You are " + age + " years old!");
+
+if (age >= 16) {
+  console.log("Good job, I think")
+} else {
+  console.log("Leave immediately")
+}
+
+if (age < 18 && === "John") {
+  console.log("You're supposed to be in jail!")
+}
+
+if (age < 18 || age > 80) {
+  console.log("Seriously, you again?")
+}
+
+let raining = False;
+if (!raining) {
+  console.log('Today is a sunny day.');
+}
+
+let animal = 'bird'
+if (animal === 'dog' || animal === 'cat' || animal === 'cow') {
+  console.log("They're on the fair");
+}
+
+const functionName = () => {
+  
+}
