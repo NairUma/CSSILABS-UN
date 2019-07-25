b2c26a86a1d271eb676a2a26c051bef792504fd0
Uma Nair <nair003@csusm.edu>
2019-07-25 11:59:18 -0700

    more M2-L4

---
+++ WeLearn/M2-javaScript/L4-JSLoop-DOM/loop/script.js
@@ -0,0 +1,28 @@
+const names = ['Alice', 'Bob', 'Charlie', 'Deborah', 'Evan'];
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+
+for (let i = 0; i < 5; i++) {
+  console.log(names[i]);
+}
+
+const books = ['Ari & Dante', 'An Enchantment of Ravens', 'The Tethered Mage',
+'These Witches Dont Burn', 'Stuff Matters'];
+books.forEach((book) => {
+  console.log(book);
+})
+
+let sum = 0;
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const findTotal = ((item) => {
+  sum = sum + item;
+  console.log(sum);
+});
+
+ /* console.log(names[0]);
+*/
