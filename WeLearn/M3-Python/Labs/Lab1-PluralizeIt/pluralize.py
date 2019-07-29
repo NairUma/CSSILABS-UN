4ae0c6a6638e8d52399ca79cbe090146ad9c3f54
Uma Nair <nair003@csusm.edu>
2019-07-29 14:24:42 -0700

    yoooo

--- WeLearn/M3-Python/Labs/Lab1-PluralizeIt/pluralize.py
+++ WeLearn/M3-Python/Labs/Lab1-PluralizeIt/pluralize.py
@@ -6,20 +6,28 @@ def plurali():
             user_word = raw_input("Word: ")
             if user_number == 1:
                 print(str(user_number) + " " + user_word)
+
             elif user_word[-2:] == "sh" or user_word[-2:] == "ch":
                 print(str(user_number) + " " + user_word + "es")
+
             elif user_word[-2:] == "us":
                 print(str(user_number) + " " + user_word[:-2] + "i")
+
             elif user_word[-2:] == "ay" or user_word[-2:] == "oy":
                 print(str(user_number) + " " + user_word[:-1] + "s")
+
             elif user_word[-2:] == "ey" or user_word[-2:] == "uy":
                 print(str(user_number) + " " + user_word[:-1] + "s")
+
             elif user_word[-1] == "y":
                 print(str(user_number) + " " + user_word[:-1] + "ies")
+
             elif user_word[-3:] == "ife":
                 print(str(user_number) + " " + user_word[:-3] + "ives")
+
             else:
                 print(str(user_number) + " " + user_word + "s")
+                
             user_dat = raw_input("Type 'Y' to try again: ")
             if user_dat == "Y":
                 user_sat = True
