2734d43510346a17f686fba8cbbb4ded1423390b
Uma Nair <nair003@csusm.edu>
2019-07-31 13:29:22 -0700

    dictionary

---
+++ WeLearn/M3-Python/L2-Python_list_Dict/dict.py
@@ -0,0 +1,16 @@
+states = {
+    "NY": "New York",
+    "CA": "California",
+    "TX": "Texas",
+    "MA": "Massachutts"
+}
+
+for abbrev in states:
+    print(abbrev + " is short for " + states[abbrev])
+
+profile = {
+    "name": "Uma"
+    "haircolor": "Black"
+    "age": 18
+    "hobby": "writing"
+}
